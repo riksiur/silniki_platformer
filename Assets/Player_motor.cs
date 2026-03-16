3ce6c6e950b7c93a2dcace4f37b8727f41c65c3d
Zofia Błoch <chie.00028@gmail.com>
2026-03-16 10:30:08 +0100

    player_motor
    możliwość zmieniania prędkości z edytora unreal

--- Assets/Player_motor.cs
+++ Assets/Player_motor.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Player_motor : MonoBehaviour
 {
+    Vector2 direction;
+    public float speed =10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,14 +14,13 @@ public class Player_motor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
     }
 
     void OnMove(InputValue value)
     {
         //Debug.Log("Move");
         // Debug.Log(value.Get<Vector2>());
-        Vector2 direction = value.Get<Vector2>();
-        transform.position += new Vector3(direction.x, direction.y, 0);
+        direction = value.Get<Vector2>();
     }
 }
