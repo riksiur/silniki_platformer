979d387658a2e4b0658b0e73fa519866d179d24f
Zofia Błoch <chie.00028@gmail.com>
2026-03-16 09:16:46 +0100

    player_motor

---
+++ Assets/Player_motor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_motor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        Debug.Log("Move");
+    }
+}
