74e95e86cef7e508c21dc6e5175a0b36955d25ea
Zofia Błoch <chie.00028@gmail.com>
2026-03-25 10:15:52 +0100

    spike zadający obrażenia

---
+++ Assets/Health_Component.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Health_Component : MonoBehaviour
+{
+    private float health = 10;
+    public float maxHealth = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
