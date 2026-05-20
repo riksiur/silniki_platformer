0c5a411dc8656e966714ee86fd50ab7601cc4874
Zofia Błoch <chie.00028@gmail.com>
2026-05-20 08:44:09 +0200

    dash
    pod f

--- Assets/scripts/Player_motor.cs
+++ Assets/scripts/Player_motor.cs
@@ -1,77 +1,96 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class playermotor : MonoBehaviour
+public class Player_motor : MonoBehaviour
 {
     Vector2 direction;
-    Rigidbody2D rigidbody2d;
+    Rigidbody2D rigidbody2D;
     public float speed = 10;
-    public float jumpforce = 5;
-    public float maxspeed = 5;
-    public float stoppingforce = 7;
+    public float jumpForce = 10;
+    public float maxSpeed = 5;
+    public float stoppingForce = 5;
+    public float dashforce = 10;
     private bool canJump = true;
-    private bool canDoubleJump = true;
+    private bool canDash = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+    void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
-    {
-        HandlePlayerMovement();
-        MaxSpeedLimiting();
-    }
-    private void HandlePlayerMovement()
+    void FixedUpdate()
     {
         if (direction.x != 0)
         {
-            rigidbody2d.AddForce(new Vector2(direction.x * speed, 0));
+            rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
         }
-        else if (rigidbody2d.linearVelocityX != 0)
+        else if (rigidbody2D.linearVelocity.x != 0)
         {
-            //zatrzymywanie
-            rigidbody2d.AddForce(new Vector2(-rigidbody2d.linearVelocityX * stoppingforce, 0));
-        }
-    }
-    private void MaxSpeedLimiting()
-    {
-        if (rigidbody2d.linearVelocityX >= maxspeed)
-        {
-            rigidbody2d.linearVelocityX = maxspeed;
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
         }
 
-        else if (rigidbody2d.linearVelocityX <= -maxspeed)
+
+        if (!canDash)
         {
-            rigidbody2d.linearVelocityX = -maxspeed;
+            return;
+        }
+        if (rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = maxSpeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = -maxSpeed;
         }
         //transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
     }
+
     void OnMove(InputValue value)
     {
-        // Debug.Log("Move");
-        //Debug.Log(value.Get<Vector2>());
+        //Debug.Log("Move");
+        // Debug.Log(value.Get<Vector2>());
         direction = value.Get<Vector2>();
     }
+
 
     private void OnJump()
     {
         if (canJump)
         {
-            rigidbody2d.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             canJump = false;
         }
-        else if (canDoubleJump)
+
+    }
+
+    private void OnDash()
+    {
+        if (canDash)
         {
-            rigidbody2d.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-            canDoubleJump = false;
+            if (direction.x != 0)
+            {
+                rigidbody2D.AddForce(new Vector2(direction.x * dashforce, 0), ForceMode2D.Impulse);
+            }
+            else
+            {
+                rigidbody2D.AddForce(new Vector2(dashforce, 0), ForceMode2D.Impulse);
+            }
+            canDash = false;
+            StartCoroutine(ResetDash(1));
         }
     }
+
+    IEnumerator ResetDash(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+        canDash = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
-        canDoubleJump = true;
     }
 }
